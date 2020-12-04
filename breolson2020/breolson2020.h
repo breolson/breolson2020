9592725a542379f8935e0d9c6e3d3f52518ea3bc
nichkov <nichkov_m@utkonos.ru>
2020-12-04 21:53:35 +0300

    initial commit

---
+++ breolson2020/breolson2020.h
@@ -0,0 +1,18 @@
+//
+//  breolson2020.h
+//  breolson2020
+//
+//  Created by Михаил Ничков on 04.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for breolson2020.
+FOUNDATION_EXPORT double breolson2020VersionNumber;
+
+//! Project version string for breolson2020.
+FOUNDATION_EXPORT const unsigned char breolson2020VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <breolson2020/PublicHeader.h>
+
+
